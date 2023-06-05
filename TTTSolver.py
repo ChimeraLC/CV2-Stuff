0da04ea2df86cbbc87e09cb084d50e04d8f35f48
ChimeraLC <lucaschen314@gmail.com>
2023-06-05 18:16:31 -0500

    realtime solving

---
+++ TTTSolver.py
@@ -0,0 +1,84 @@
+def Solve(grid):
+	threes = [[0,1,2], [3,4,5], [6,7,8], 
+	   [0,3,6], [1,4,7], [2,5,8], 
+	   [0,4,8], [2, 4, 6]]
+	# Figure out whose move it is
+	difcount = 0
+	player = 0
+	for val in grid:
+		if (val == "o"):
+			difcount -=1
+		if (val == "x"):
+			difcount += 1
+	# x turn
+	if difcount == 0:
+		player = 1
+	# y turn
+	elif difcount == 1:
+		player = 0
+	else:
+		print("Unviable board")
+		return -1
+	
+	# calculating all the positions
+	"""
+	for index in len(threes):
+	for i in range(3):
+		threes[index[i]] = grid[threes[index[i]]]
+	"""
+	# looking for wins	
+	for t in threes:
+		if (sorted([grid[i] for i in t]) == [player, player, 2]):
+			for i in t:
+				if grid[i] == 2:
+					return i
+	# preventing enemy wins
+	for t in threes:
+		if (sorted([grid[i] for i in t]) == [1-player, 1-player, 2]):
+			for i in t:
+				if grid[i] == 2:
+					return i
+	# finding forks
+	for pos in range(9):
+		# Test each possible move for forks
+		if grid[pos] != 2:
+			continue
+		gridcopy = grid.copy()
+		gridcopy[pos] = player
+		if FindMatches(gridcopy, [player, player, 2]) >= 2:
+			return pos
+	return -1
+	
+
+# Defines how good a position is for a player
+# 3- won 2 - preventing loss 3 - forced win 4 - setup win 5 - aim towards center
+def Score(grid, player):
+	# Winning states
+    if FindMatches(grid, [player, player, player]) >= 1:
+	    return 3
+
+# Finds opportunites for wins
+def FindMatches(grid, distribution):
+	ret = 0
+	# rows 
+	for i in range(3):
+		if sorted(grid[i*3:i*3+3]) == distribution:
+			ret += 1
+	# columns
+	for j in range(3):
+		if sorted([grid[j], grid[3+j], grid[6+j]]) == distribution:
+			ret += 1
+	# diagonals
+	if sorted([grid[0], grid[4], grid[8]]) == distribution:
+		ret += 1
+	if sorted([grid[2], grid[4], grid[6]]) == distribution:
+		ret += 1
+	return ret
+
+threes = [[0,1,2], [3,4,5], [6,7,8], 
+	   [0,3,6], [1,4,7], [2,5,8], 
+	   [0,4,8], [2, 4, 6]]
+grid = [2, 2, 2, 1, 2, 1, 0, 0, 2]
+
+#print(sorted([grid[i] for i in [0,1,2]]))
+#print(Solve([1, 0, 0, 2, 2, 1, 2, 2, 2]))
